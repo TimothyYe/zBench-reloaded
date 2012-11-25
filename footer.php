f4043ad5bcd347fc9ed3fc280256aa6838f0cfef
Timothy <yexiaozhou2003@gmail.com>
2012-11-25 20:10:15 +0800

    adjust footer

--- footer.php
+++ footer.php
@@ -1,10 +1,10 @@
-</div><!--wrapper-->
+    </div><!--wrapper-->
 <div class="clear"></div>
 <div id="footer">
 	<div id="footer-inside">
 		<p>
 			<?php _e('Copyright', 'zbench'); ?> &copy; <?php echo date("Y"); ?> <?php bloginfo('name'); ?>
-			| Powered by <a href="http://wordpress.org/">WordPress</a> and <a href="http://zww.me">zBench</a>
+			| Powered by <a href="http://wordpress.org/">WordPress</a> and <a href="http://zww.me">zBench</a>   <script language="javascript" src="http://count24.51yes.com/click.aspx?id=242284288&logo=1" charset="gb2312"></script> <?php echo get_num_queries(); ?> queries in <?php timer_stop(3); ?> seconds
 		</p>
 		<span id="back-to-top">&uarr; <a href="#" rel="nofollow" title="Back to top"><?php _e('Top', 'zbench'); ?></a></span>
 	</div>
